c4f1deaf0849c614fe74f466ecf16c797edcfada
masihNoob <neraka.malam@gmail.com>
2018-03-01 18:01:23 +0700

    belum fix

---
+++ Assets/Scripts/GameplayManager.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameplayManager : MonoBehaviour {
+    public static int gridWidth = 11, gridHeight = 20;
+	public static Transform[,]grid = new Transform[gridWidth, gridHeight];
+	
+	private void Start() {
+		GenerateMino();
+	}
+	private string GetRandomMino(){
+		int val = Random.Range(0,7);
+		string minoName = "MinoT";
+		switch (val)
+		{
+			
+			case 0:
+				minoName = "MinoL";
+			break;
+			case 1:
+				minoName = "MinoJ";
+			break;
+			case 2:
+				minoName = "MinoI";
+			break;
+			case 3:
+				minoName = "MinoO";
+			break;
+			case 4:
+				minoName = "MinoS";
+			break;
+			case 5:
+				minoName = "MinoZ";
+			break;
+			case 6:
+				minoName = "MinoT";
+			break;
+		}
+		return "Prefabs/"+minoName;
+	}
+	public Transform GetTransformAtGridPosition(Vector3 pos){
+		if (pos.y > gridHeight - 1)
+			return null;
+		else
+			return grid[(int)pos.x, (int)pos.y];
+	}
+	public void UpdateGrid(SystemHandler termino){
+		for (int y = 0; y < gridHeight; y++)
+		{
+			for (int x = 0; x < gridWidth; x++)
+			{
+				if(grid[x,y]!=null){
+					if(grid[x,y].parent == termino.transform)
+					grid[x,y] = null;
+				}
+			}
+			
+		}
+		foreach (Transform mino in termino.transform)
+		{
+			Vector3 pos= Round(mino.position);
+			if(pos.y < gridHeight)
+				grid[(int)pos.x, (int)pos.y] =mino;
+		}
+	}
+	public void GenerateMino(){
+		GameObject termino = (GameObject)Instantiate(Resources.Load(GetRandomMino(),typeof(GameObject)), new Vector3(5.0f, 18.0f, 0.0f),Quaternion.identity);
+	}
+
+	
+	public bool TermoInsideGrid(Vector3 pos){
+		return(
+			(int)pos.x >=1 &&
+			(int)pos.x < gridWidth &&
+			(int)pos.y >=1
+		);
+	}
+	public Vector3 Round(Vector3 pos){
+		return new Vector3(
+			Mathf.Round(pos.x),
+			Mathf.Round(pos.y),
+			Mathf.Round(pos.z)
+		);
+	}
+    private bool IsRowFullAtint(int y)
+    {
+		for (int x = 0; x < gridWidth; x++)
+		{
+			if(grid[x,y]==null)
+			return false;
+		}
+		return true;
+    }
+    private void DestroyRowAt(int y)
+    {
+        for (int x = 0; x < gridWidth; x++)
+        { 
+			Destroy(grid[x,y].gameObject);
+			grid[x,y] =null;
+        }
+
+    }
+    private void MoveRowDown(int y)
+    {
+        for (int x = 0; x < gridWidth; x++)
+        {
+            if (grid[x, y] != null){
+				grid[x,y - 1] = grid[x,y];
+				grid[x,y] = null;
+				grid[x,y - 1].position += Vector3.down;
+			}       
+        }
+    }
+    private void MoveAllRowDowns(int y)
+    {
+		for (int i =y; i < gridHeight; i++)
+			MoveRowDown(i);
+    }
+    public void DestroyRow()
+    {
+        for (int y = 0; y < gridHeight; y++)
+        {
+			if(IsRowFullAtint(y)){
+                DestroyRowAt(y);
+				MoveAllRowDowns(y+1);
+				y--;
+			}
+			
+        }
+    }
+}
---
+++ Assets/Scripts/SystemHandler.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SystemHandler : MonoBehaviour {
+
+	
+	private GameplayManager gameplaymanager;
+	[SerializeField]
+	private float fallSpeed = 1;
+	[SerializeField]
+	private bool allowRotation = true;
+	[SerializeField]
+	private bool limitRotation = false;
+	private float fall = 0;
+	// Use this for initialization
+	void Start () {
+		gameplaymanager = FindObjectOfType<GameplayManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		InputKeyHandler();
+		UpdateMino();
+	}
+
+	private void InputKeyHandler(){
+		if(Input.GetKeyDown(KeyCode.RightArrow))
+			Handler("Right");
+		else if (Input.GetKeyDown(KeyCode.LeftArrow))
+            Handler("Left");
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+            Handler("Down");
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+            Handler("Action");
+	}
+	private void UpdateMino(){
+		if(Time.time - fall >= fallSpeed){
+			Handler("Down");
+			fall = Time.time;
+		}
+	}
+	private void Handler(string command)
+	{
+		switch (command)
+		{
+			case "Right":
+				MoveHorizontal(Vector3.right);
+			break;
+			case "Left":
+            	MoveHorizontal(Vector3.left);
+			break;
+			case "Down":
+				MoveVertical();
+			break;
+			case "Action":
+				if(allowRotation){
+					ActionLimitRotataion(1);
+					if(!IsValidPosition())
+						ActionLimitRotataion(-1);
+					else
+						gameplaymanager.UpdateGrid(this);
+				}
+			break;
+		}
+	}
+	private void ActionLimitRotataion(int modifier){
+		if(limitRotation){
+			if(transform.rotation.eulerAngles.z >=90)
+				transform.Rotate(Vector3.forward* -90);
+			else
+                transform.Rotate(Vector3.forward * 90 );
+		}else
+		{
+            transform.Rotate(Vector3.forward * 90 * modifier);
+		}
+	}
+	private void MoveVertical(){
+		transform.position += Vector3.down;
+		if(!IsValidPosition()){
+            transform.position += Vector3.up;
+			gameplaymanager.DestroyRow();
+			enabled = false;
+			gameplaymanager.GenerateMino();
+		}else
+            gameplaymanager.UpdateGrid(this);
+	}
+	private void MoveHorizontal(Vector3 direction){
+		transform.position += direction;
+		if(!IsValidPosition())
+            transform.position += direction * -1;
+        else
+            gameplaymanager.UpdateGrid(this);
+	}
+	private bool IsValidPosition(){
+		foreach (Transform mino in transform){
+			Vector3 pos = gameplaymanager.Round(mino.position);
+			if(!gameplaymanager.TermoInsideGrid(pos))
+			return false;
+			if (gameplaymanager.GetTransformAtGridPosition(pos)!=null && gameplaymanager.GetTransformAtGridPosition(pos).parent != transform){
+				return false;
+			}
+		}return true;
+		
+	}
+
+	
+}
